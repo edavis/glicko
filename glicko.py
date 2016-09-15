04fa62f93a8b62ff9c35db7a3473c061a81c2089
Eric Davis <edavis@eresources.com>
2016-09-15 10:31:28 -0700

    Initial commit

---
+++ glicko.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from math import log, sqrt, pi
+
+q = round(log(10.0) / 400.0, 7)
+
+def g(RD):
+    _ = 1.0 / sqrt(1.0 + 3.0 * q ** 2.0 * (RD ** 2.0) / pi ** 2.0)
+    return round(_, 4)
+
+def E(r, rj, RDj):
+    _ = 1.0 / (1.0 + pow(10.0, -g(RDj) * (r - rj) / 400.0))
+    return round(_, 3)
+
+def d2(r, matches):
+    _ = (q ** 2.0 * sum((g(RDj) ** 2.0) * (E(r, rj, RDj)) * (1 - E(r, rj, RDj)) for (rj, RDj, sj) in matches)) ** -1.0
+    return round(_, 2)
+
+def glicko(r, RD, matches):
+    r_new = r + q / (1.0 / RD ** 2.0 + 1.0 / d2(r, matches)) * sum(g(RDj) * (sj - E(r, rj, RDj)) for (rj, RDj, sj) in matches)
+    RD_new = sqrt((1.0 / RD ** 2.0 + 1.0 / d2(r, matches)) ** -1.0)
+    return (round(r_new, 0), round(RD_new, 1))
---
+++ test_glicko.py
@@ -0,0 +1,21 @@
+import glicko
+
+def test_q():
+    assert glicko.q == 0.0057565
+
+def test_g():
+    assert glicko.g(30) == 0.9955
+    assert glicko.g(100) == 0.9531
+    assert glicko.g(300) == 0.7242
+
+def test_E():
+    assert glicko.E(1500, 1400, 30) == 0.639
+    assert glicko.E(1500, 1550, 100) == 0.432
+    assert glicko.E(1500, 1700, 300) == 0.303
+
+def test_d2():
+    assert glicko.d2(1500, [(1400, 30, 1), (1550, 100, 0), (1700, 300, 0)]) == 53670.85
+
+def test_glicko():
+    matches = [(1400, 30, 1), (1550, 100, 0), (1700, 300, 0)]
+    assert glicko.glicko(1500, 200, matches) == (1464, 151.4)
